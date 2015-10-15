4de5cf0cf41cc2317c393b424b39f698f5036f4b
agraboso <agraboso@gmail.com>
2015-10-14 21:10:00 -0400

    Implement error handling

---
+++ src/errors.js
@@ -0,0 +1,53 @@
+/**
+ * Error class for an RSAA that does not conform to the RSAA definition
+ *
+ * @class InvalidRSAA
+ * @access public
+ * @param {array} validationErrors - an array of validation errors
+ */
+class InvalidRSAA extends Error {
+  constructor(validationErrors) {
+    super();
+    this.name = 'InvalidRSAA';
+    this.message = 'Invalid RSAA';
+    this.validationErrors = validationErrors;
+  }
+}
+
+/**
+ * Error class for an error raised trying to make an API call
+ *
+ * @class RequestError
+ * @access public
+ * @param {string} message - the error message
+ */
+class RequestError extends Error {
+  constructor(message) {
+    super();
+    this.name = 'RequestError';
+    this.message = message;
+  }
+}
+
+/**
+ * Error class for an API response outside the 200 range
+ *
+ * @class ApiError
+ * @access public
+ * @param {number} status - the status code of the API response
+ * @param {string} statusText - the status text of the API response
+ * @param {object} response - the parsed JSON response of the API server if the
+ *  'Content-Type' header signals a JSON response
+ */
+class ApiError extends Error {
+  constructor(status, statusText, response) {
+    super();
+    this.name = 'ApiError';
+    this.status = status;
+    this.statusText = statusText;
+    this.response = response;
+    this.message = `${status} - ${statusText}`;
+  }
+}
+
+export { InvalidRSAA, RequestError, ApiError };
